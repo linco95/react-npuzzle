07a75f8ab4572101ceabf0250b94668c3b052237
Andreas Kjellqvist <linco95@gmail.com>
2020-06-24 23:12:39 +0200

    Cleaning up the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import "./normalize.css"
 import './App.css';
+import Header from "./components/Header"
+import Game from "./components/Game"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header title="15 Puzzle" />
+      <main>
+        <h1>15 Puzzle</h1>
+        <Game boardSize={4} />
+      </main>
     </div>
   );
 }
